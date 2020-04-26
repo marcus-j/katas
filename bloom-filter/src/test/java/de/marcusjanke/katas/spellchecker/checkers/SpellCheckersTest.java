6e8d5bdeddf2d1f3f1c1f2d972883f43dead83c3
marcus-j <18111840+marcus-j@users.noreply.github.com>
2020-04-26 20:22:12 +0200

    asserts that items from the entire loaded dictionary return positive results

--- bloom-filter/src/test/java/de/marcusjanke/katas/spellchecker/checkers/SpellCheckersTest.java
+++ bloom-filter/src/test/java/de/marcusjanke/katas/spellchecker/checkers/SpellCheckersTest.java
@@ -3,6 +3,7 @@ package de.marcusjanke.katas.spellchecker.checkers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static de.marcusjanke.katas.spellchecker.checkers.SpellCheckers.dictionaryBasedSpellChecker;
 import static de.marcusjanke.katas.spellchecker.dictionaries.Dictionaries.lineSeparatedStringResourceDictionary;
 import static de.marcusjanke.katas.spellchecker.engines.Engines.bloomFilterEngine;
 import static de.marcusjanke.katas.spellchecker.engines.HashFunctions.MURMUR_3;
@@ -17,8 +18,17 @@ class SpellCheckersTest {
     @DisplayName("should create DictionaryBasedSpellChecker with convenience method")
     void shouldCreateDictionaryBasedSpellChecker() {
         final String resourcePath = "/dictionary/wordlist.txt";
-        final SpellChecker actualSpellChecker = SpellCheckers.dictionaryBasedSpellChecker(MAX_SIZE, MURMUR_3, resourcePath, ISO_8859_1);
+        final SpellChecker actualSpellChecker = dictionaryBasedSpellChecker(MAX_SIZE, MURMUR_3, resourcePath, ISO_8859_1);
 
         assertThat(actualSpellChecker).isEqualTo(new DictionaryBasedSpellChecker(bloomFilterEngine(MAX_SIZE, MURMUR_3), lineSeparatedStringResourceDictionary(resourcePath, ISO_8859_1)));
+    }
+
+    @Test
+    @DisplayName("should return true for all dictionary items")
+    void shouldReturnTrueForAllDictionaryItems() {
+        final String resourcePath = "/dictionary/wordlist.txt";
+        final SpellChecker actualSpellChecker = dictionaryBasedSpellChecker(MAX_SIZE, MURMUR_3, resourcePath, ISO_8859_1);
+
+        assertThat(lineSeparatedStringResourceDictionary(resourcePath, ISO_8859_1).stream().anyMatch(word -> !actualSpellChecker.check(word))).isFalse();
     }
 }
