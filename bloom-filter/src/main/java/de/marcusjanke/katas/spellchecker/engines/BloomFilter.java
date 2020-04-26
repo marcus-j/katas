d929e514d6b257d5479bc219d4c0669c0c3d2e82
marcus-j <18111840+marcus-j@users.noreply.github.com>
2020-04-26 12:49:28 +0200

    adds basic bloom filter

---
+++ bloom-filter/src/main/java/de/marcusjanke/katas/spellchecker/engines/BloomFilter.java
@@ -0,0 +1,29 @@
+package de.marcusjanke.katas.spellchecker.engines;
+
+import java.util.BitSet;
+import java.util.function.ToIntFunction;
+
+class BloomFilter<T> {
+
+    private final BitSet bitSet;
+    private final int maxSize;
+    private final ToIntFunction<T> hashFunction;
+
+    BloomFilter(int maxSize, ToIntFunction<T> hashFunction) {
+        this.bitSet = new BitSet(maxSize);
+        this.maxSize = maxSize;
+        this.hashFunction = hashFunction;
+    }
+
+    void putElement(T element) {
+        bitSet.set(normalizedHash(element));
+    }
+
+    boolean mightContainElement(T element) {
+        return bitSet.get(normalizedHash(element));
+    }
+
+    private int normalizedHash(T element) {
+        return hashFunction.applyAsInt(element) % maxSize;
+    }
+}
--- bloom-filter/src/test/java/de/marcusjanke/katas/spellchecker/engines/BloomFilterTest.java
+++ bloom-filter/src/test/java/de/marcusjanke/katas/spellchecker/engines/BloomFilterTest.java
@@ -1,16 +1,37 @@
 package de.marcusjanke.katas.spellchecker.engines;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.function.ToIntFunction;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class BloomFilterTest {
 
+    private static final ToIntFunction<String> HASH_BY_STRING_LENGTH_FUNCTION = String::length;
+
     @Test
+    @DisplayName("should apply a given hash function for updating the underlying bit set")
     void shouldApplyHashFunction() {
-        final ToIntFunction<String> hashFunction = String::length;
-        var bloomFilter = new BloomFilter<>(2, hashFunction);
+        final BloomFilter<String> bloomFilter = new BloomFilter<>(3, HASH_BY_STRING_LENGTH_FUNCTION);
+        bloomFilter.putElement("");
+        bloomFilter.putElement("1");
 
+        assertThat(bloomFilter.mightContainElement("")).isTrue();
+        assertThat(bloomFilter.mightContainElement("1")).isTrue();
+        assertThat(bloomFilter.mightContainElement("11")).isFalse();
+    }
 
+    @Test
+    @DisplayName("should limit bit set size and truncate longer hashes by applying a modulo operation. Hence, setting 'wrongish' bits")
+    void shouldLimitBitSetSize() {
+        final BloomFilter<String> bloomFilter = new BloomFilter<>(2, HASH_BY_STRING_LENGTH_FUNCTION);
+        bloomFilter.putElement("1");
+        bloomFilter.putElement("11");
+
+        assertThat(bloomFilter.mightContainElement("")).isTrue();
+        assertThat(bloomFilter.mightContainElement("1")).isTrue();
+        assertThat(bloomFilter.mightContainElement("11")).isTrue();
     }
 }
